5d8ce1ba1d6d8f27c138d46b945aa3bdce976c34
aevonso <oddonenso@gmail.com>
2026-02-21 15:59:11 +0300

    feath/start realisation beatiful error

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from '../App.vue'
 import routes from './routes/routes'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 import '../css/app.css'
 
 const pinia = createPinia()
@@ -14,6 +16,22 @@ const router = createRouter({
 
 const app = createApp(App)
 
+const toastOptions = {
+  position: 'top-right',
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl:false
+}
+
+app.use(Toast, toastOptions)
 app.use(pinia)
 app.use(router)
 
